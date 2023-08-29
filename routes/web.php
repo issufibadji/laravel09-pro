19eb037c9ea6f8633f866e9c4afd1c22cca72602
trf/conibj <issufi.badji@tty2000.com.br>
2023-08-29 17:59:29 -0300

    12-fallback

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/**11 - Agrupando rotas com subdominio*/
+/**12 - fallback*/
 
-Route::domain('{user}.example.com')->group(function () {
 
-    Route::get('/user/{id}/{name}',function ($id = null, $name = null) {
-        return ' User ' . $id . ' - ' . $name;
-    });
-
-    Route::get('/user/{id}', function ($id) {
-        return 'User '.$id;
-    });
+Route::get('/user/{id}', function ($id) {
+    return 'User '.$id;
 });
 
+//entra em acção quando nenuma dadas rotas não foram encontrada
 
-
+Route::fallback(function () {
+    return 'Alternativa rota!';
+});
