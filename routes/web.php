005d2f4b7229803bb33172c44442069b4349ea79
trf/conibj <issufi.badji@tty2000.com.br>
2023-08-29 14:51:19 -0300

    6-Rotas com parâmetros

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-/** criar route usando view direto em vez de get*/
-//direcionar view diretamente da rota
+/** 6-Rotas com parâmetros */
 
-Route::view('/welcome','welcome',[
-    'title'=>'Hello World'
-]);
+
+Route::get('/user/{id?}',function () {
+    return 'User';
+});
+// saida : http://localhost:8000/user/id
+
+Route::get('/user/{id}', function ($id) {
+    return 'User '.$id;
+});
+// saida : http://localhost:8000/user/02
+
+
+Route::get('/user/{id}/{name}',function ($id = null, $name = null) {
+    return ' User ' . $id . ' - ' . $name;
+});
+// saida : http://localhost:8000/user/02/laravel
 
 Route::get('/',function (){
     return view('welcome');
 });
+
+// saiida : http://localhost:8000/user/02/laravel
 
