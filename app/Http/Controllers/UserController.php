7f151504a08c6de99b78e74507746ef754da48e7
trf/conibj <issufi.badji@tty2000.com.br>
2023-08-30 17:28:46 -0300

    4-Injetando model no controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,20 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-
     public function index()
     {
         dd('x');
     }
 
-    public function show($id, Request $request)
+    public function show(Request $request, User $user)
     {
+        //return $user;
+        dd($user); //Digite no url:http://127.0.0.1:8000/user/1
 
-        dd($request->query('firstname'), $id); //Digite no url:http://127.0.0.1:8000/user/1?firstname=Issufi
-       // dd('show', $id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\UserController;
 
-/**3 - Injeção de dependência*/
+/**4 - Injetando model no controller*/
 
 Route::get('/users', [UserController::class, 'index']);
 
-Route::get('/user/{id}', [UserController::class, 'show']);
+Route::get('/users/{user}', [UserController::class, 'show']);
 
 
 Route::fallback(function () {
