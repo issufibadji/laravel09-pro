0191f3843c3c2e089dd6ef30a869070c1812d78f
trf/conibj <issufi.badji@tty2000.com.br>
2023-08-31 15:22:39 -0300

    6-Controller de ação única

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @method static find(int $int)
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\UserController;
 
-/**5 - Aplicando middleware*/
+
+/**6 - Controller de ação única*/
 
 Route::get('/users', [UserController::class, 'index']);
 
 Route::get('/users/{user}', [UserController::class, 'show']);
 
-
-Route::fallback(function () {
-    return 'Alternativa rota!';
-});
+Route::get('/checkout/{token}', CheckoutController::class);
+//
+//Route::fallback(function () {
+//    return 'Alternativa rota!';
+//});
