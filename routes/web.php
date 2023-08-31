4304b5a1a88e0d4459497cf2fd227dc561f38867
trf/conibj <issufi.badji@tty2000.com.br>
2023-08-31 16:57:46 -0300

    7-Resource controller-criando resources

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
-use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\UserController;
 
 
 /**6 - Controller de ação única*/
 
-Route::get('/users', [UserController::class, 'index']);
+Route::resource('/users', UserController::class);
 
-Route::get('/users/{user}', [UserController::class, 'show']);
-
-Route::get('/checkout/{token}', CheckoutController::class);
-//
-//Route::fallback(function () {
-//    return 'Alternativa rota!';
-//});
