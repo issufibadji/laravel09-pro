46e8ba6b1319723f052462927aa9c374f5372aea
trf/conibj <issufi.badji@tty2000.com.br>
2023-08-31 18:24:50 -0300

    13 - Resource controllers - renomeando parâmetros

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,16 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\UserController;
 
 
-/**12 - Resource controllers - nomeando rotas*/
+/**13 - Resource controllers - renomeando parâmetros*/
 
-Route::resource('users', UserController::class)->names([
-    'create'=>'usuario.criar',
-    'update'=>'usuario.atualizar',
+Route::resource('users', UserController::class)->parameters([
+    'users' => 'admin_user'
 ]);
+
+///users/{admin_user}
+
+Route::resource('users.commets', UserController::class)->parameters([
+    'users' => 'admin_user',
+    'comments' => 'admin_comment',
+    ]);
 
