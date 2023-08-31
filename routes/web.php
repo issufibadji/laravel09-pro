c7e953eece38b366c639c6c8aaed28b2ef4f10f8
trf/conibj <issufi.badji@tty2000.com.br>
2023-08-31 17:22:16 -0300

    9-Resource controllers - múltiplos resources

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,10 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\UserController;
 
 
-/**8 - Resource controllers - definindo métodos na rota*/
+/**9 - Resource controllers - múltiplos resources*/
 
-Route::resource('/users', UserController::class)->only([
-    'index', 'show'
-]);
+Route::resources([
+    'users', UserController::class,
+    'posts', UserController::class,
 
-Route::resource('/users', UserController::class)->except([
-    'create', 'store', 'update', 'destroy'
 ]);
