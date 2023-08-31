7edb20578a7a42ea30de0fb8633de8b057171eda
trf/conibj <issufi.badji@tty2000.com.br>
2023-08-31 17:42:53 -0300

    10-Resource controllers - resource para api

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ use \App\Http\Controllers\UserController;
 
 
 /**9 - Resource controllers - múltiplos resources*/
+Route::resource('/users', UserController::class)->except(['create', 'edit']);
 
-Route::resources([
+Route::apiResource('users',UserController::class);//é igual a except(['create', 'edit']);
+
+Route::apiResources([
     'users', UserController::class,
     'posts', UserController::class,
-
 ]);
